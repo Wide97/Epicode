8ab06eadd18092c1e9b35b88a6d12ea903841501
Wide97 <mwidesott@gmail.com>
2024-06-20 14:48:50 +0200

    Lezione 4

--- CLASSE-FS0624/Mese1/Setimana2/Giorno4/assets/js/D4.js
+++ CLASSE-FS0624/Mese1/Setimana2/Giorno4/assets/js/D4.js
@@ -2,7 +2,7 @@
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
 */
 
-function area(l1, l2){
+function area(l1, l2) {
     return l1 * l2;
 }
 
@@ -14,21 +14,51 @@ console.log(area(7, 9));
  la loro somma moltiplicata per tre.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function crazySum(num1, num2) {
+    if (num1 === num2) {
+        return (num1 + num2) * 3;
+    } else {
+        return num1 + num2;
+    }
+}
+
+console.log(crazySum(6, 6));
+console.log(crazySum(9, 3));
 
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta (pari) tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function crazyDiff(num1) {
+    let differenza = num1 - 19;
+    if (num1 > 19) {
+        return differenza * 3;
+    } else {
+        return differenza;
+    }
+}
+
+console.log(crazyDiff(10));
+console.log(crazyDiff(23));
+console.log(crazyDiff(20));
 
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function boundary(n) {
+    if ((n >= 20 && n <= 100) || n === 400)
+        return true;
+    else {
+        return false;
+    }
+}
+
+console.log(boundary(67));
+console.log(boundary(178));
+console.log(boundary(400));
 
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
