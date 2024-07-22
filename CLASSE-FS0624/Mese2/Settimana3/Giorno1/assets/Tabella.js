2bcab80fdcf498f18b3546fffdf0c8d7f3af829b
Wide97 <mwidesott@gmail.com>
2024-07-22 16:52:43 +0200

    w3 g1

--- CLASSE-FS0624/Mese2/Settimana3/Giorno1/assets/Tabella.js
+++ CLASSE-FS0624/Mese2/Settimana3/Giorno1/assets/Tabella.js
@@ -43,7 +43,7 @@ document.addEventListener('submit', function(e){
 
     for (let i=0; i < PetList.length; i++) {
         const newLi = document.createElement('li')
-        newLi.innerText= PetList[i].nomeAnimale + '' + PetList[i].nomePadrone + ''+ PetList[i].specie + 'della razza' + PetList[i].razzaAnimale
+        newLi.innerText= PetList[i].nomeAnimale + ' ' + PetList[i].nomePadrone + ' ' + PetList[i].specie + ' della razza ' + PetList[i].razzaAnimale
         newLi.classList.add('list-group-item')
         unorderedList.appendChild(newLi)
     }
