89c1fbe571623dec9c58cb487ad3e06c04501890
Wide97 <mwidesott@gmail.com>
2024-07-22 16:39:31 +0200

    w3 g1

--- CLASSE-FS0624/Mese2/Settimana3/Giorno1/assets/Tabella.js
+++ CLASSE-FS0624/Mese2/Settimana3/Giorno1/assets/Tabella.js
@@ -15,10 +15,27 @@ document.addEventListener('submit', function(e){
     const specieValue = specieInput.value
     const razzaAnimaleValue = razzaAnimaleInput.value
 
-    console.log (nomeAnimaleValue)
+    console.log(nomeAnimaleValue)
     console.log(nomePadroneValue)
     console.log(specieValue)
     console.log(razzaAnimaleValue)
+
+    const element = new Pet(nomeAnimaleValue, nomePadroneValue, specieValue, razzaAnimaleValue)
+    console.log(element)
+
+    PetList.push(element)
+
+    const unorderedList = document.getElementById('animals-list')
+
+    unorderedList.innerHTML = ''
+
+    for (let i=0; i < PetList.length; i++) {
+        const newLi = document.createElement('li')
+        newLi.innerText= PetList[i].nomeAnimale + '' + PetList[i].nomePadrone + ''+ PetList[i].specie + 'della razza' + PetList[i].razzaAnimale
+        newLi.classList.add('list-group-item')
+        unorderedList.appendChild(newLi)
+    }
+    document.getElementsByTagName('form')[0].reset()
 
 
  
