28e6dabfd5fe4177dea7bec67f66dcb8557f2784
Wide97 <mwidesott@gmail.com>
2024-07-23 16:05:37 +0200

    w3 d2

--- CLASSE-FS0624/Mese2/Settimana3/Giorno2/assets/timer.js
+++ CLASSE-FS0624/Mese2/Settimana3/Giorno2/assets/timer.js
@@ -1,6 +1,6 @@
 const timerCount = document.getElementById('time-counter')
 
-const startCount = sessionStorage.getItem('startCount') ? parseInt(sessionStorage.getItem('startCount')) :Date.now()
+const startCount = sessionStorage.getItem('startCount') ? parseInt(sessionStorage.getItem('startCount')) : Date.now()
 
 function updateCounter (){
     const currentTime = Date.now()
