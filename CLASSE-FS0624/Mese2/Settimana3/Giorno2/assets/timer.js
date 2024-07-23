3d783b6c2749a3ad2ebb73f2814fa9bbae704458
Wide97 <mwidesott@gmail.com>
2024-07-23 16:32:01 +0200

    w3 d2

--- CLASSE-FS0624/Mese2/Settimana3/Giorno2/assets/timer.js
+++ CLASSE-FS0624/Mese2/Settimana3/Giorno2/assets/timer.js
@@ -1,16 +1,24 @@
-const timerCount = document.getElementById('time-counter')
+const timerCount = document.getElementById('time-counter');
 
-const startCount = sessionStorage.getItem('startCount') ? parseInt(sessionStorage.getItem('startCount')) : Date.now()
+const startCount = sessionStorage.getItem('startCount') ? parseInt(sessionStorage.getItem('startCount')) : Date.now();
 
-function updateCounter (){
-    const currentTime = Date.now()
-    const seconds = Math.floor((currentTime - startCount) / 1000)
-    timerCount.textContent =  seconds
+function updateCounter() {
+    const currentTime = Date.now();
+    const seconds = Math.floor((currentTime - startCount) / 1000);
+    timerCount.textContent = seconds;
 }
 
-window.onload = function () {
-    setInterval(updateCounter, 1000)
+function animateCounter() {
+    timerCount.classList.add('scale-up');
+    
+    setTimeout(() => {
+        timerCount.classList.remove('scale-up');
+    }, 500); // Durata dell'animazione
 }
 
-
-    sessionStorage.setItem('startCount', startCount)
+window.onload = function() {
+    setInterval(updateCounter, 1000); 
+    setInterval(animateCounter, 1000); 
+    
+    sessionStorage.setItem('startCount', startCount);
+}
