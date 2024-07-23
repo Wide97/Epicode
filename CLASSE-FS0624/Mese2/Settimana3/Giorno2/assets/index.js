3a8eb8a5c7fc53979b09dedfc03e937a7e1f119d
Wide97 <mwidesott@gmail.com>
2024-07-23 15:36:45 +0200

    w3 d2

--- CLASSE-FS0624/Mese2/Settimana3/Giorno2/assets/index.js
+++ CLASSE-FS0624/Mese2/Settimana3/Giorno2/assets/index.js
@@ -1,4 +1,26 @@
-const saveButton =document.getElementById('save')
+const saveButton = document.getElementById('save')
 const resetButton = document.getElementById('reset')
 
 const textarea = document.getElementById('notepad-textarea')
+const names = document.getElementById('names')
+
+const save = function () {
+    console.log('cliccato')
+    const textareaValue = textarea.value
+    names.textContent = textareaValue
+    localStorage.setItem('notepad-content', textareaValue)
+}
+
+saveButton.addEventListener('click', save)
+
+const reset = function() {
+    names.textContent = ''
+    textareaValue = ''
+    localStorage.removeItem('notepad-content')
+    alert('rimosso')
+}
+
+resetButton.addEventListener('click', reset)
+
+
+
