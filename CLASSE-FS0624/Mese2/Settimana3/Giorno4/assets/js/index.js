e7538965ae43d938b8e9284b129e22774849495a
Wide97 <mwidesott@gmail.com>
2024-07-25 15:21:49 +0200

    w3 d4

--- CLASSE-FS0624/Mese2/Settimana3/Giorno4/assets/js/index.js
+++ CLASSE-FS0624/Mese2/Settimana3/Giorno4/assets/js/index.js
@@ -1,5 +1,19 @@
-fetch('https://api.pexels.com/v1/search?query=kittens', {
+fetch('https://api.pexels.com/v1/search?query=hamsters', {
     headers: {
       Authorization: "eHOVxlqKjj0SslycsG9VRKow49Mg7BFCMOwxuHxmpQx2RFYChl7GUI0d"
     }
+  })
+  .then((response) => {
+    console.log(response)
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw new Error('errore')
+    }
+  })
+  .then((data) => {
+    console.log('DATA', data)
+  })
+  .catch((err) => {
+    console.log(err)
   })
